1d654715b75c1bae69b6a76a1b3279547df0730f
Naftali-B <nbudamba@gmail.com>
2024-05-28 23:27:58 +0300

    all

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" Babel setup """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config(object):
+    """ Babel Configuration """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ uses request.accept_languages
+
+        Return: determined best match with supported languages.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """ 
+        simply outputs “Welcome to Holberton” as page title (<title>)
+        and “Hello world” as header (<h1>
+    """
+    return render_template('2-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
